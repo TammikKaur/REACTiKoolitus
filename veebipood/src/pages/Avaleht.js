a46d58562cd884a2ed78ca716fd4ba28a7a3c5c9
TammikKaur <tammikkaur@gmail.com>
2022-05-31 16:16:24 +0300

    Update Avaleht.js

--- veebipood/src/pages/Avaleht.js
+++ veebipood/src/pages/Avaleht.js
@@ -2,12 +2,19 @@ import { useState } from "react";
 
 function Avaleht() {
     //let synniaeg = "1913.12.31";
+
+    //useState laseb muuta htmli sees olevaid v22rtusi. muidu REACT ei muuda enam p2rast htmli osa
+    //et kui korra on juba 2ra laadinud html, siis isegi kui mingi v22rtus muutub, siis seda ei kuva
+    //htmlis ja pead kasutama useState, mis siis ekstra l2heb kontrollib yle k6ik koodi, et kuskohas
+    //vastava nimega muutuja v6iks olla ja uuendab 2ra selle v22rtuse.
     const isikukood = "41513123113";
+    //esialgu annad mingi algv22rtuse useStatele, hiljem saad muuta
     const [synniaeg, uuendaSynniaega] = useState("1913.12.31");
     const [s6naline, uuendaS6naline] = useState("algv22rtus");
     const [numbriline, uuendaNumbriline] = useState(123456);
     const [kahendv22rtus, uuendaKahendV22rtus] = useState(true);
     
+    //v6tab isikukoodist kindlad numbrid ja kuvab nende j2rgi synniaja
     const muudaSynniaega = () => {
         //synniaeg = "3213213121";
         uuendaSynniaega("19" + isikukood.substring(1,3) + "." + isikukood.substring(3,5) + "." + isikukood.substring(5,7));
